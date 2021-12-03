494b3452434a1d4bb64594d85bf16c476b0fa498
Felix Roos <roos@entrecode.de>
2021-12-03 23:34:34 +0100

    about texts

--- components/AboutMore.tsx
+++ components/AboutMore.tsx
@@ -19,42 +19,87 @@ export default function AboutMore() {
           {fr && 'Présentation'}
         </Heading>
         <div className="prose">
-          <p>
-            Les Ecuries du Flecken ont été rachetées en décembre 2019 par Hélène et Sylvain. Depuis, de nombreuses
-            améliorations ont été apportées, et plusieurs projets s’apprêtent encore à voir le jour.
-          </p>
-          <p>
-            Le système de litière permanente, qui existait jusqu’alors, a été totalement supprimée. Les stabulations ont
-            été repensées, et une stabulation-parcours a été aménagée. Récemment le sol du manège a également été refait
-            à neuf.
-          </p>
-          <p>
-            Passionnés depuis l’enfance et propriétaires de chevaux depuis de nombreuses années, nous nous sommes formés
-            (Licence professionnelle “Management et Gestion des entreprises de la filière équine” et Galop 7 pour
-            Hélène, accompagnateur tourisme équestre (en cours) et Galop 5 d’attelage pour Sylvain) et avons développé
-            notre savoir-faire au contact de professionnels (stage gestion de la reproduction et poulinage au Haras de
-            Hus, stage de horsemanship avec Andy Booth, …).
-          </p>
-          <p>
-            Compétiteurs dans notre jeunesse (CSO, concours complet, TREC), mais randonneurs dans l’âme, nous partageons
-            notre goût pour les chevaux, la nature, le terroir et la découverte de paysages à cheval.
-          </p>
-          <p>
-            Leidenschaftliche Reiter sind wir seit unserer Kindheit und viele Jahre halten wir nun auch selbst Pferde.
-            Seither bilden wir uns stetig weiter: Hélène hat die Berufslizenz zur Pferdewirtin („Management et Gestion
-            des entreprises de la filière équine“) und den Galop 7, Sylvain den Galop 5 im Fahren und absolviert zur
-            Zeit seine Ausbildung zum Wanderrittführer. Verfeinert haben wir unser Savoir-faire während diverser
-            Praktika im professionellen Bereich, wie im Zuchtbetrieb des Haras de Hus und beim Horsemanship-Lehrgang bei
-            Andy Booth.
-          </p>
-          <p>
-            In unserer Jugend haben wir unser Können auf Springturnieren, Vielseitigkeits- und Orientierungsritten
-            gemessen, im Herzen sind wir aber Wanderreiter.
-          </p>
-          <p>
-            Unsere Freude an Pferden und unsere Liebe zur Natur möchten wir mit Ihnen teilen und hoch zu Ross die Gegend
-            entdecken und die traumhaften Landschaften der Nordvogesen genießen.
-          </p>
+          {fr && (
+            <>
+              <p>
+                Les Ecuries du Flecken ont été rachetées en décembre 2019 par Hélène et Sylvain. Depuis, de nombreuses
+                améliorations ont été apportées, et plusieurs projets s’apprêtent encore à voir le jour.
+              </p>
+              <p>
+                Le système de litière permanente, qui existait jusqu’alors, a été totalement supprimée. Les stabulations
+                ont été repensées, et une stabulation-parcours a été aménagée. Récemment le sol du manège a également
+                été refait à neuf.
+              </p>
+            </>
+          )}
+          {de && (
+            <p>
+              Im Dezember 2019 haben Hélène und Sylvain die Ecuries du Flecken übernommen. Seitdem wurden bereits einige
+              Verbesserungen vorgenommen und und viele neue Projekte stehen noch an. Das bis dahin bestehende System der
+              permanenten Einstreu wurde eingstellt, die Stallungen neu durchdacht und ein neuer Offenstall kam dazu.
+              Vor Kurzem wurde der Hallenboden gänzlich erneuert.
+            </p>
+          )}
+          {fr && <p>Nous innovons dès que nous le pouvons pour améliorer le quotidien de nos pensionnaires !</p>}
+          {de && (
+            <p>
+              Wir erneuern und entwickeln uns stetig weiter, um den Alltag unserer Einsteller immer noch ein bisschen
+              besser zu gestalten!
+            </p>
+          )}
+
+          {fr && (
+            <p>
+              Passionnés depuis l’enfance et propriétaires de chevaux depuis de nombreuses années, nous nous sommes
+              formés (Licence professionnelle “Management et Gestion des entreprises de la filière équine” et Galop 7
+              pour Hélène, accompagnateur tourisme équestre (en cours) et Galop 5 d’attelage pour Sylvain) et avons
+              développé notre savoir-faire au contact de professionnels (stage gestion de la reproduction et poulinage
+              au Haras de Hus, stage de horsemanship avec Andy Booth, …).
+            </p>
+          )}
+          {de && (
+            <p>
+              Leidenschaftliche Reiter sind wir seit unserer Kindheit und viele Jahre halten wir nun auch selbst Pferde.
+              Seither bilden wir uns stetig weiter: Hélène hat die Berufslizenz zur Pferdewirtin („Management et Gestion
+              des entreprises de la filière équine“) und den Galop 7, Sylvain den Galop 5 im Fahren und absolviert zur
+              Zeit seine Ausbildung zum Wanderrittführer. Verfeinert haben wir unser Savoir-faire während diverser
+              Praktika im professionellen Bereich, wie im Zuchtbetrieb des Haras de Hus und beim Horsemanship-Lehrgang
+              bei Andy Booth.
+            </p>
+          )}
+          {fr && (
+            <p>
+              Compétiteurs dans notre jeunesse (CSO, concours complet, TREC), mais randonneurs dans l’âme, nous
+              partageons notre goût pour les chevaux, la nature, le terroir et la découverte de paysages à cheval.
+            </p>
+          )}
+          {de && (
+            <>
+              <p>
+                In unserer Jugend haben wir unser Können auf Springturnieren, Vielseitigkeits- und Orientierungsritten
+                gemessen, im Herzen sind wir aber Wanderreiter.
+              </p>
+              <p>
+                Unsere Freude an Pferden und unsere Liebe zur Natur möchten wir mit Ihnen teilen und hoch zu Ross die
+                Gegend entdecken und die traumhaften Landschaften der Nordvogesen genießen.
+              </p>
+            </>
+          )}
+
+          {/* <p>
+        Située à Lembach (F-67510) dans le magnifique Parc naturel régional des Vosges du Nord, notre structure à taille
+        humaine se veut avant tout être un havre de paix, où la beauté du paysage suffit à se ressourcer au contact de
+        son compagnon à sabots.
+      </p>
+      <p>Le bien-être de votre cheval ? Nous en faisons notre priorité.</p>
+      <p>
+        Passionnés depuis l’enfance et propriétaires de chevaux depuis de nombreuses années, nous nous sommes formés
+        (Licence professionnelle “Management et Gestion des entreprises de la filière équine” et Galop 7 pour Hélène,
+        accompagnateur tourisme équestre (en cours) et Galop 5 d’attelage pour Sylvain) et avons développé notre
+        savoir-faire au contact de professionnels (stage gestion de la reproduction et poulinage au Haras de Hus, stage
+        de horsemanship avec Andy Booth, …). Compétiteurs dans notre jeunesse (CSO, concours complet, TREC), mais
+        randonneurs dans l’âme, nous partageons notre goût pour les chevaux, la nature, le terroir et la découverte de
+        paysages à cheval.{' '}</p>*/}
         </div>
       </Centered>
     </>
