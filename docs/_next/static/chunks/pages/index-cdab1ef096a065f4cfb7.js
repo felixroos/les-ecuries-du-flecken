64023aeb4b6f1c1c2c7c4bfa9b7f99d0ba3bf493
Felix Roos <roos@entrecode.de>
2021-12-07 23:24:00 +0100

    export

--- docs/_next/static/chunks/pages/index-cdab1ef096a065f4cfb7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5301:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(8851)}])}},function(n){n.O(0,[603,771,384,851,774,888,179],(function(){return _=5301,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
