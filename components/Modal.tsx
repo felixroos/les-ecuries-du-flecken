10bf5558dc9248cf362a086eec802d879d65d4af
Felix Roos <roos@entrecode.de>
2022-01-21 15:00:15 +0100

    dont focus first link

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,12 +1,16 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
 export default function Modal({ open, onClose, children }) {
+  let focus = useRef(null);
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={onClose}>
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={onClose} initialFocus={focus}>
+        <div
+          className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+          ref={focus}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
