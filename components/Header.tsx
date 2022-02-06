fb95ab9cebe3a4544de7f72af944a723b833596c
Felix Roos <roos@entrecode.de>
2022-02-06 17:29:38 +0100

    img lazy loading by default

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,3 +25,18 @@ export default function Header({ small }: any) {
   );
   /* </Parallax> */
 }
+
+{
+  /* <header style={{ height: small ? '280px' : '600px' }} className="overflow-hidden relative">
+<div className="absolute -top-32 aspect-auto h-full w-[1280] h-[933]">
+  <Image src="/img/header2.jpg" alt="Les Écuries du Flecken" width={1280} height={933} />
+</div>
+<div className="px-6 md:text-right pt-4 md:pt-8 absolute top-0 right-0">
+  <h1 className="text-4xl md:text-5xl text-primary">Les Écuries du Flecken</h1>
+  <h2 className="text-2xl md:text-3xl text-primary">
+    {de && 'Wanderreiten & Pferdepension im Elsass'}
+    {fr && 'Pension et Tourisme Èquestre en Alsace'}
+  </h2>
+</div>
+</header> */
+}
--- components/Img.tsx
+++ components/Img.tsx
@@ -1,6 +1,6 @@
 function Img(props) {
   const src = props.src.startsWith('/') ? `.${props.src}` : props.src;
-  return <img {...props} src={src} />;
+  return <img {...props} src={src} loading="lazy" />;
 }
 
 export default Img;
