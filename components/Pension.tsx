357e29a3bbc2b53f2f21f517557e000907983386
Felix Roos <flix91@gmail.com>
2025-03-09 09:07:29 +0100

    fix: thin out pension section

--- components/Pension.tsx
+++ components/Pension.tsx
@@ -1,127 +1,70 @@
-import React from 'react';
-import { useLocales } from '../pages/api/useLocales';
-import Anchor from './Anchor';
-import ContactButton from './ContactButton';
-import Heading from './Heading';
-import Img from './Img';
-import offenstall from '../public/img/offenstall.jpg.webp';
-import jaziaschlafend from '../public/img/jaziaschlafend.jpg.webp';
-import enjoy from '../public//img/enjoy.jpg.webp';
+import React from "react";
+import { useLocales } from "../pages/api/useLocales";
+import Anchor from "./Anchor";
+import ContactButton from "./ContactButton";
+import Heading from "./Heading";
+import Img from "./Img";
+import offenstall from "../public/img/offenstall.jpg.webp";
+import jaziaschlafend from "../public/img/jaziaschlafend.jpg.webp";
+import enjoy from "../public//img/enjoy.jpg.webp";
 
 const Spacer = () => <div className="py-4 md:py-6"></div>;
-const Divider = () => <div className={`border-b-2 border-gray-100 py-2 md:py-6`} />;
-const Centered = ({ children }) => <div className="flex flex-col md:items-center">{children}</div>;
+const Divider = () => (
+  <div className={`border-b-2 border-gray-100 py-2 md:py-6`} />
+);
+const Centered = ({ children }) => (
+  <div className="flex flex-col md:items-center">{children}</div>
+);
 const Slim = ({ children }) => <div className="max-w-lg">{children}</div>;
 
 export default function Pension() {
   const [fr, de] = useLocales();
   return (
     <>
-      <Anchor id={'pension'} />
+      <Anchor id={"pension"} />
       <Centered>
         <Heading>Pension</Heading>
         <div className="prose">
-          <Img src={offenstall.src} className="rounded-xl mx-auto mb-4" alt="" layout="responsive" />
-          {fr && <p>Pour chevaux et poneys, à partir de 280 € / mois.</p>}
-          {de && <p>Für Pferde und Ponies, ab 280€ / Monat.</p>}
-          {fr && <p>Différents types d’hébergement:</p>}
-          {de && <p>Verschiedene Unterbringungsmöglichkeiten </p>}
-          <ul>
-            {fr && <li>En box (au minimum 3,5m x 4m – pouvant aller jusqu’à 5m x 5m)</li>}
-            {de && <li>In der Box (von mindestens 3,5m x 4m bis 5m x5m)</li>}
-            {fr && <li>En stabulation intérieure avec sciure et filet à foin</li>}
-            {de && <li>Im Laufstall auf Späne mit Heunetz</li>}
-            {fr && <li>En stabulation ouverte sur parcours</li>}
-            {de && <li>Im Offenstall</li>}
-          </ul>
-          {fr && <p>Services compris:</p>}
-          {de && <p>Wir leisten:</p>}
-          <p></p>
-          <ul>
-            <li>
-              {fr && <p>Foin matin et soir (entièrement produit sur la ferme)</p>}
-              {de && <p>Heu morgens und abends (von unseren Wiesen)</p>}
-            </li>
-            {fr && (
-              <li>
-                Distribution d&apos;1 L d’orge aplati (ou aliment de la gamme St Hippolyt à un prix avantageux en
-                supplément) - la distribution d’un aliment fourni par le propriétaire est non facturé. Les CMV –
-                compléments minéraux et vitaminés - sont à fournir par le propriétaire (une commande groupée possible)
-              </li>
-            )}
-            {de && (
-              <li>
-                Fütterung mit 1l gequetschter Gerste (oder Futter der Marke St Hippolyt zum vergünstigten Preis)
-                Fütterung von eigenem Futter ist kostenlos. Mineral-und Vitaminfutter werden vom Besitzer selbst besorgt
-                (eine Gruppenbestellung bei Reverdy ist möglich)
-              </li>
-            )}
-            {fr && <li>Mise à disposition de bloc de sel pour chaque cheval</li>}
-            {de && <li>Salzlecksteine zur freien Verfügung </li>}
-            {fr && <li>Abreuvoirs anti-gel</li>}
-            {de && <li>Frostsichere Tränken</li>}
-            {fr && <li>Casier individuel</li>}
-            {de && <li>Persönlicher Spind</li>}
-            {fr && <li>Accès au manège éclairé et mise à disposition de plots, barres, chandeliers...</li>}
-            {de && <li>Zugang zur lichtdurchfluteten Reithalle mit Stangen und anderem Bodenarbeitszubehör</li>}
-            {fr && <li>Chemins de randonnées attenants à l&apos;écurie</li>}
-            {de && <li>Zugang zu Reitwegen im Wald (Ausweis für den Zugang zum Wald)</li>}
-            {fr && <li>Sorties quotidiennes en hiver</li>}
-            {de && <li>Täglicher Auslauf im Winter</li>}
-          </ul>
-
-          {fr && <p>Tous nos pensionnaires ont accès aux pâtures afin de vivre leur vie de cheval. </p>}
-          {de && <p>Alle Pferde haben Zugang zu den Weiden um ein artgerechtes Leben führen zu können.</p>}
-          {fr && (
-            <p>
-              Nous offrons une vie en troupeau au minimum 6 mois dans l’année à nos pensionnaires. Chaque nouveau cheval
-              est intégré progressivement. Pour les chevaux à problèmes (boiterie, embonpoint, fourbure,...), un accès
-              restreint selon les besoins physiologiques est possible avec intégration dans un troupeau adapté.{' '}
-            </p>
-          )}
-          {de && (
-            <p>
-              Wir bieten unseren Pensionspferden mindestens 6 Monate Herdenhaltung auf der Weide im Jahr. Jedes neue
-              Pferd wird langsam in die bestehende Herde integriert. Für Pferde mit körperlichen Problemen (Lahmheit,
-              Übergewicht, Hufrehe...) besteht die Möglichkeit auf begrenzten Weidezugang in einer passenden Herde.
-            </p>
-          )}
-          {/* {fr && (
-            <p>
-              A la fin de la belle saison (mi-octobre, début novembre), les chevaux regagnent progressivement leurs
-              quartiers d’hiver. Les sorties – comprises dans le prix de pension – sont quotidiennes en hiver (en
-              paddock ou manège suivant la météo).
-            </p>
-          )}
-          {de && (
-            <p>
-              Geht die milde Jahreszeit zu Ende (Mitte Oktober, Anfang November), ziehen die Pferde langsam in ihr
-              Winterquartier um. Je nach Wetter dürfen die Pferde täglich zusammen auf den Paddock oder in die Halle.
-            </p>
-          )} */}
+          <Img
+            src={offenstall.src}
+            className="rounded-xl mx-auto mb-4"
+            alt=""
+            layout="responsive"
+          />
+          {fr && <p>Renseignements et tarifs, nous consulter</p>}
+          {de && <p>Auskünfte und Preise auf Anfrage</p>}
         </div>
         <ContactButton>
-          {de && 'Jetzt kontaktieren'}
-          {fr && 'Contactez nous'}
+          {de && "Jetzt kontaktieren"}
+          {fr && "Contactez nous"}
         </ContactButton>
         <div className="prose">
           {fr && <h2>Pension retraite et pension poulains/jeunes chevaux</h2>}
           {de && <h2>Pension für Rentnerpferde oder Fohlen / Jungpferde</h2>}
-          <Img src={jaziaschlafend.src} className="rounded-xl mx-auto mb-4" alt="" layout="responsive" />
+          <Img
+            src={jaziaschlafend.src}
+            className="rounded-xl mx-auto mb-4"
+            alt=""
+            layout="responsive"
+          />
           {fr && (
             <p>
-              Vous souhaitez nous confier votre compagnon à quatre pattes afin qu’il soit entre de bonnes mains,
-              accompagné et bichonné pour une retraite paisible ? Vous souhaitez que votre poulain – ou jeune
-              cheval - profite d&apos;une vie avec ses congénères, en troupeau pendant sa croissance ? Nous mettons
-              notre savoir-faire au service de votre animal, et avons exactement ce dont vous et, surtout, votre cheval,
-              avez besoin.
+              Vous souhaitez nous confier votre compagnon à quatre pattes afin
+              qu’il soit entre de bonnes mains, accompagné et bichonné pour une
+              retraite paisible ? Vous souhaitez que votre poulain – ou jeune
+              cheval - profite d&apos;une vie avec ses congénères, en troupeau
+              pendant sa croissance ? Nous mettons notre savoir-faire au service
+              de votre animal, et avons exactement ce dont vous et, surtout,
+              votre cheval, avez besoin.
             </p>
           )}
           {de && (
             <p>
-              Möchten Sie Ihren vierbeinigen Freund während seiner Rente in guten Händen wissen? Oder möchten Sie, dass
-              ihr Fohlen / Jungpferd mit seinen Artgenossen in der Herde aufwächst? Wir setzen für Ihr Tier unser ganzes
-              Savoir Faire ein und geben ihm genau das was es braucht.
+              Möchten Sie Ihren vierbeinigen Freund während seiner Rente in
+              guten Händen wissen? Oder möchten Sie, dass ihr Fohlen / Jungpferd
+              mit seinen Artgenossen in der Herde aufwächst? Wir setzen für Ihr
+              Tier unser ganzes Savoir Faire ein und geben ihm genau das was es
+              braucht.
             </p>
           )}
           {/* <p>Une pension tout compris à un tarif préférentiel ! (renseignements au 06 99 93 64 34 )</p> */}
@@ -132,75 +75,112 @@ export default function Pension() {
             {de && <li>Wurmkur 2 mal im Jahr</li>}
             {fr && (
               <li>
-                Foin à volonté (filet à foin pour réguler les plus gourmands, et étaler l’affouragement sur la journée)
-              </li>
-            )}
-            {de && <li>Heu nach Belieben (in Heunetzen, um die Mahlzeit auf den Tag zu verteilen)</li>}
-            {fr && (
-              <li>
-                Aliments adaptés aux besoins du cheval fournis (âge, corpulence, ...) de la gamme St Hippolyt.
-                Compléments minéraux et vitaminés (en supplément).
+                Foin à volonté (filet à foin pour réguler les plus gourmands, et
+                étaler l’affouragement sur la journée)
               </li>
             )}
             {de && (
               <li>
-                Mineral- und Vitaminfutter oder eine speziell an die Bedürfnisse des Pferdes (Alter, Körperbau...)
-                angepasste Ernährung mit Futtermitteln von St Hippolyt
+                Heu nach Belieben (in Heunetzen, um die Mahlzeit auf den Tag zu
+                verteilen)
+              </li>
+            )}
+            {fr && (
+              <li>
+                Aliments adaptés aux besoins du cheval fournis (âge, corpulence,
+                ...) de la gamme St Hippolyt. Compléments minéraux et vitaminés
+                (en supplément).
+              </li>
+            )}
+            {de && (
+              <li>
+                Mineral- und Vitaminfutter oder eine speziell an die Bedürfnisse
+                des Pferdes (Alter, Körperbau...) angepasste Ernährung mit
+                Futtermitteln von St Hippolyt
               </li>
             )}
             {fr && <li>Parage des sabots max. 8 fois par an</li>}
             {de && <li>Ausschneiden der Hufe bis zu acht mal im Jahr</li>}
-            {fr && <li>Soins d&apos;entretien hebdomadaire (brossage, nettoyage des sabots)</li>}
-            {de && <li>Wöchentliche Körperpflege (Bürsten, Hufe auskratzen...)</li>}
+            {fr && (
+              <li>
+                Soins d&apos;entretien hebdomadaire (brossage, nettoyage des
+                sabots)
+              </li>
+            )}
+            {de && (
+              <li>Wöchentliche Körperpflege (Bürsten, Hufe auskratzen...)</li>
+            )}
             {fr && <li>Soins vétérinaires</li>}
             {de && <li>Tierärztliche Versorgung</li>}
             {fr && (
               <li>
-                Manipulation du poulain/jeune cheval (désensibilisation - renforcement positif) et travail éthologique
-                au sol (selon les besoins – embarquement en van, ...){' '}
+                Manipulation du poulain/jeune cheval (désensibilisation -
+                renforcement positif) et travail éthologique au sol (selon les
+                besoins – embarquement en van, ...){" "}
               </li>
             )}
             {de && (
               <li>
-                Arbeit mit dem Fohlen oder Jungpferd (Desensibilisierung durch positive Verstärkung) Bodenarbeit nach
-                Bedarf, z.B. Verladetraining
+                Arbeit mit dem Fohlen oder Jungpferd (Desensibilisierung durch
+                positive Verstärkung) Bodenarbeit nach Bedarf, z.B.
+                Verladetraining
               </li>
             )}
-            {fr && <li>Nouvelles hebdomadaires de votre cheval (vidéos et photos)</li>}
-            {de && <li>Wöchentlich Neuigkeiten in Form von Videos und Fotos</li>}
             {fr && (
               <li>
-                Hébergement en stabulation – parcours, ou stabulation intérieure avec accès au pré. Vie en groupe.
+                Nouvelles hebdomadaires de votre cheval (vidéos et photos)
               </li>
             )}
-            {de && <li>Unterbringung im Offenstall mit Zugang zur Koppel. Leben in der Herde.</li>}
+            {de && (
+              <li>Wöchentlich Neuigkeiten in Form von Videos und Fotos</li>
+            )}
+            {fr && (
+              <li>
+                Hébergement en stabulation – parcours, ou stabulation intérieure
+                avec accès au pré. Vie en groupe.
+              </li>
+            )}
+            {de && (
+              <li>
+                Unterbringung im Offenstall mit Zugang zur Koppel. Leben in der
+                Herde.
+              </li>
+            )}
           </ul>
         </div>
         <ContactButton>
-          {de && 'Preise auf Anfrage'}
-          {fr && 'Tarifs sur demande'}
+          {de && "Preise auf Anfrage"}
+          {fr && "Tarifs sur demande"}
         </ContactButton>
         <div className="prose">
           {fr && <h2>Pension juments gestantes - poulinage</h2>}
           {de && <h2>Pension für trächtige Stuten -Abfohlüberwachung </h2>}
-          <Img src={enjoy.src} className="rounded-xl mx-auto mb-4" alt="" layout="responsive" />
+          <Img
+            src={enjoy.src}
+            className="rounded-xl mx-auto mb-4"
+            alt=""
+            layout="responsive"
+          />
           {fr && (
             <p>
-              Pour préparer la venue d’un poulain en toute sérénité. Nous nous chargeons de la surveillance et de
-              l’accompagnement des juments gestantes, leur prodiguant les soins nécessaires. Et ce jusqu’au poulinage et
-              sevrage du poulain.
+              Pour préparer la venue d’un poulain en toute sérénité. Nous nous
+              chargeons de la surveillance et de l’accompagnement des juments
+              gestantes, leur prodiguant les soins nécessaires. Et ce jusqu’au
+              poulinage et sevrage du poulain.
             </p>
           )}
           {de && (
             <p>
-              Um die Ankunft eine Fohlens in aller Ruhe vorzubereiten, begleiten und überwachen wir trächtige Stuten und
-              gewährleisten so die nötige Pflege. Und das bis zum Abfohlen und späteren Absetzen des Fohlens.
+              Um die Ankunft eine Fohlens in aller Ruhe vorzubereiten, begleiten
+              und überwachen wir trächtige Stuten und gewährleisten so die
+              nötige Pflege. Und das bis zum Abfohlen und späteren Absetzen des
+              Fohlens.
             </p>
           )}
         </div>
         <ContactButton>
-          {de && 'Preise auf Anfrage'}
-          {fr && 'Tarifs sur demande'}
+          {de && "Preise auf Anfrage"}
+          {fr && "Tarifs sur demande"}
         </ContactButton>
       </Centered>
     </>
